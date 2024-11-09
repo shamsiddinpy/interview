018f483a9814085e9f1dc6e3a643e52583f97342
shamsiddindaminov <shamsiddinpython215@gmail.com>
2024-11-09 21:10:10 +0500

    605 Easy

---
+++ Array/58. Length of Last Word.py
@@ -0,0 +1,6 @@
+# s = "   fly me   to   the moon  "
+s = "Hello how are you Contestant"
+k = 4
+length = []
+words = s.split(" ")
+print(" ".join(words[0:k]))
--- LeetCode 75/605. Can Place Flowers.py
+++ LeetCode 75/605. Can Place Flowers.py
@@ -1,4 +1,4 @@
 flowerbed = [1, 0, 0, 0, 1]
 n = 1
-
+b = len(flowerbed)
 
