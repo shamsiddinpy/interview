a0b83215d81ecb9acbd6ac42fda47cb105afc488
shamsiddindaminov <shamsiddinpython215@gmail.com>
2024-11-10 12:13:38 +0500

    605 Easy

--- Array/66. Plus One.py
+++ Array/66. Plus One.py
@@ -1,4 +1,9 @@
-digits = [1, 2, 3]
-plus = 0
-for digit in digits:
-  pass
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i in range(len(digits) - 1, -1, -1):
+    if digits[i] < 9:
+        digits[i] += 1
+        print(digits)
+    digits[i] = 0
+digits.insert(0, 1)
+print(digits)
