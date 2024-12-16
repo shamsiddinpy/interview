86f77dea80757eb2b29c18686ba9e5d5d4ec54ec
shamsiddindaminov <shamsiddinpython215@gmail.com>
2024-12-16 14:05:16 +0500

    context manager...

--- Nazariy bilimlar/context menejer/context.py
+++ Nazariy bilimlar/context menejer/context.py
@@ -1,2 +1,11 @@
-with open('context.txt', 'w') as file:
-    file.write("Context menejer  context.text fayiliga w orqali yoziladiii..!!!")
+# with open('context.json', 'w') as file:
+#     file.write("Context menejer  context.text fayiliga w orqali yoziladiii..!!!")
+
+import json
+
+with open('context.json', 'r') as file:
+    context1 = file.read()
+    if not context1.strip():
+        raise Exception('The context1 file is empty')
+    data = json.loads(context1)
+    print(data)
